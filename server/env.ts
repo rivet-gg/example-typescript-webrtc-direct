e93a6c22fe76a8275ba8a0099c957286bc2a7d72
Nathan Flurry <developer@nathanflurry.com>
2022-09-12 23:40:39 +0000

    Init rivet integration

---
+++ server/env.ts
@@ -0,0 +1,10 @@
+if (
+	!process.env.PORT_signaling ||
+	!process.env.PORT_RANGE_MIN_webrtc ||
+	!process.env.PORT_RANGE_MAX_webrtc
+)
+	throw "Missing env var: PORT_signaling, PORT_RANGE_MIN_webrtc, PORT_RANGE_MAX_webrtc";
+
+export const PORT_SIGNALING = parseInt(process.env.PORT_signaling);
+export const PORT_WEBRTC_MIN = parseInt(process.env.PORT_RANGE_MIN_webrtc);
+export const PORT_WEBRTC_MAX = parseInt(process.env.PORT_RANGE_MAX_webrtc);
