835fd693cfb365fd41c462e317446c1132ae5365
Nathan Flurry <developer@nathanflurry.com>
2022-09-13 21:20:25 +0000

    Expose empty env in prod config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,2 +1,8 @@
-module.exports = require("./webpack.base.config.js");
+const webpack = require("webpack");
+
+let config = require("./webpack.base.config.js");
+config.plugins.push(new webpack.DefinePlugin({
+    'process.env': "{}"
+}));
+module.exports = config;
 
