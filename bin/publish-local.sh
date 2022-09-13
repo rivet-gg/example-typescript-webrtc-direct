d2b1fcb8e35b99b4d3aa4f3eb1a88b122c26c045
Nathan Flurry <developer@nathanflurry.com>
2022-09-13 21:20:44 +0000

    Update client build script in publish

--- bin/publish-local.sh
+++ bin/publish-local.sh
@@ -6,6 +6,7 @@ export RIVET_CLOUD_API_URL=https://cloud.api.rivet-gg.test/v1
 docker build -t example-webrtc .
 ../cli/target/debug/rivet build push --tag example-webrtc --name "$(date +%s)"
 
-npm run build:client
+rm -rf dist
+npm run build:client:prod
 ../cli/target/debug/rivet site push --path dist/ --name "$(date +%s)"
 
