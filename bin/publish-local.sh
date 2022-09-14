0ba0cf663b7c4e310fa564941a1b97e456f343d4
Nathan Flurry <developer@nathanflurry.com>
2022-09-14 03:01:24 +0000

    Remove hardcoded url

--- bin/publish-local.sh
+++ bin/publish-local.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -euf
 
-export RIVET_CLOUD_API_URL=https://cloud.api.rivet-gg.test/v1
-
 docker build -t example-webrtc .
 ../cli/target/debug/rivet build push --tag example-webrtc --name "$(date +%s)"
 
